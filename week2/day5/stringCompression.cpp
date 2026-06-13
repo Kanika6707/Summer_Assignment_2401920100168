50817c2399314d9c14fddbea56272c424c827dce
Kanika6707 <kanikasgh007@gmail.com>
2026-06-13 18:47:43 +0530

    Create stringCompression.cpp

---
+++ week2/day5/stringCompression.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n=chars.size();
+        int idx=0;
+        for(int i=0;i<n;i++){
+            char ch=chars[i];
+            int count=0;
+
+        
+        while(i<n && chars[i]==ch){
+            count++; i++;
+        }
+        if(count==1){
+            chars[idx++]=ch;
+        }
+        else{
+            chars[idx++]=ch;
+            string st=to_string(count);
+            for(char dig:st){
+                chars[idx++]=dig;
+            }
+        }
+        i--;
+    } 
+    chars.resize(idx);
+    return idx;   
+
+
+
+
+        
+    }
+};
